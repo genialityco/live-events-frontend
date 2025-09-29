52ee4379edb504be0440ada94193ff943c609075
juanfe <lassojuanfe@gmail.com>
2025-09-29 17:51:06 -0500

    Actualiza uri de backend

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_URL = 'https://belongs-broken-becomes-indexes.trycloudflare.com'; 
+export const API_URL = 'https://passion-corrections-brian-listed.trycloudflare.com'; 
 
 export const api = axios.create({
   baseURL: API_URL,
